ae4ff5e3b0f7227b8c173c1171f589bf2eb9dc21
kac44 <kac4484@gmail.com>
2025-09-12 10:45:36 +0900

    20250912

---
+++ src/main/java/com/portoria/PortoriaApplication.java
@@ -0,0 +1,13 @@
+package com.portoria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortoriaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortoriaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/portoria/PortoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortoriaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
